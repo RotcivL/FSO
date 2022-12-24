f4472e2c83d5e8fe2235acb3880e06e969121600
RotcivL <victor.lee.98.vic@gmail.com>
2022-12-24 13:52:34 +1100

    Ex1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,6 +17,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + bad + neutral;
 
   return (
     <div>
@@ -24,10 +25,14 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text={"good"} />
       <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
       <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+
       <Header header={"statistics"} />
       <Display text={"good"} value={good} />
       <Display text={"neutral"} value={neutral} />
       <Display text={"bad"} value={bad} />
+      <Display text={"all"} value={total} />
+      <Display text={"average"} value={(good - bad) / total} />
+      <Display text={"positive"} value={(good * 100) / total + "%"} />
     </div>
   );
 };
