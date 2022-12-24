9166f61c2cc10df66f3362826a79988e85b332a8
RotcivL <victor.lee.98.vic@gmail.com>
2022-12-24 11:44:49 +1100

    ex1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,16 +5,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
   );
 };
 
@@ -37,7 +39,7 @@ const App = () => {
   const exercises3 = 14;
 
   return (
-    <>
+    <div>
       <Header course={course} />
       <Content
         part1={part1}
@@ -52,7 +54,7 @@ const App = () => {
         exercises2={exercises2}
         exercises3={exercises3}
       />
-    </>
+    </div>
   );
 };
 
