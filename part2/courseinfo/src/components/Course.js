17ab104657faafd7fb87fdcf2c694e7560c19e4b
RotcivL <victor.lee.98.vic@gmail.com>
2022-12-26 13:40:53 +1100

    Ex2.2 + 2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,14 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 
 const Course = ({ course }) => {
+  const sum = course.parts.reduce((total, part) => total + part.exercises, 0);
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total sum={sum} />
     </div>
   );
 };
