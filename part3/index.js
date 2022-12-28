a4e5485468b55cf8429bdb7c8aa52b730f3b4029
RotcivL <victor.lee.98.vic@gmail.com>
2022-12-28 11:25:05 +1100

    Ex.3.3

--- part3/index.js
+++ part3/index.js
@@ -28,10 +28,22 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+});
+
 app.get("/info", (request, response) => {
   const info = `<div><p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p></div>`;
   response.send(info);
 });
+
+
 const PORT = 3001;
 app.listen(PORT, (request, response) => {
   console.log(`Server running on port ${PORT}`);
