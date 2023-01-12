34d4346f5b9f1c21a35dc8abc69a1cd5a481549c
RotcivL <victor.lee.98.vic@gmail.com>
2023-01-13 10:58:12 +1100

    Ex.3.9

--- part3/index.js
+++ part3/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+
+app.use(cors());
+
 const morgan = require("morgan");
 morgan.token("body", function (request, response) {
   if (request.method === "POST") {
