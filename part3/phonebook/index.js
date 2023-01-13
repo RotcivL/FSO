27759e1c30d6d6ec5041e9369cd0dbcd9ffc569e
RotcivL <victor.lee.98.vic@gmail.com>
2023-01-13 14:00:48 +1100

    Ex.3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -44,10 +44,9 @@ app.get("/info", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((n) => n.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id).then((result) => {
+    response.status(204).end();
+  });
 });
 
 app.post("/api/persons/", (request, response) => {
