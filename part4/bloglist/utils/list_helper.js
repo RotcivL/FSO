9f879e6d39ad2301a0a52b58ba52954d3c551cfc
RotcivL <victor.lee.98.vic@gmail.com>
2023-01-17 19:38:04 +1100

    Ex4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, current) => total + current.likes, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  return blogs.reduce((max, current) =>
+    max.likes > current.likes ? max : current
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
