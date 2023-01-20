b02ba586027477b65e379b3732658d6c01f5bbc9
RotcivL <victor.lee.98.vic@gmail.com>
2023-01-20 14:16:46 +1100

    Ex.4.23

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const blog = require("../models/blog");
 
+let token;
 beforeEach(async () => {
-  await Blog.deleteMany({});
+  await User.deleteMany({});
 
-  await Blog.insertMany(helper.initialBlogs);
+  const passwordHash = await bcrypt.hash("secret", 10);
+  const user = new User({ username: "root", name: "super", passwordHash });
+  await user.save();
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  token = jwt.sign(userForToken, process.env.SECRET);
+
+  await Blog.deleteMany({});
+  blogs = helper.initialBlogs.map(
+    (blog) => new Blog({ ...blog, user: user.id })
+  );
+  await Blog.insertMany(blogs);
 });
 
 test("correct number of blogs are returned", async () => {
@@ -32,6 +52,7 @@ test("a valid blog can be added", async () => {
 
   await api
     .post("/api/blogs")
+    .set("Authorization", `bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
@@ -39,8 +60,27 @@ test("a valid blog can be added", async () => {
   const blogsAtEnd = await helper.blogsInDb();
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  const blogsNoId = blogsAtEnd.map(({ id, ...others }) => others);
+  const blogsNoId = blogsAtEnd.map(({ id, user, ...others }) => others);
   expect(blogsNoId).toContainEqual(newBlog);
+});
+
+test("a valid blog can not be added when no token is provided", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const newBlog = {
+    title: "new blog",
+    author: "RotcivL",
+    url: "https://newblogrotcivl.com",
+    likes: 0,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(401)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toEqual(blogsAtStart);
 });
 
 test("default likes to 0 if property is missing", async () => {
@@ -52,6 +92,7 @@ test("default likes to 0 if property is missing", async () => {
 
   const response = await api
     .post("/api/blogs")
+    .set("Authorization", `bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect("Content-Type", /application\/json/);
@@ -60,34 +101,47 @@ test("default likes to 0 if property is missing", async () => {
 });
 
 test("blog without title is not added", async () => {
+  const blogsAtStart = await helper.blogsInDb();
   const newBlog = {
     url: "https://newblogrotcivl.com",
   };
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+  await api
+    .post("/api/blogs")
+    .set("Authorization", `bearer ${token}`)
+    .send(newBlog)
+    .expect(400);
 
   const blogsAtEnd = await helper.blogsInDb();
 
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  expect(blogsAtEnd).toEqual(blogsAtStart);
 });
 
 test("blog without url is not added", async () => {
+  const blogsAtStart = await helper.blogsInDb();
   const newBlog = {
     title: "new blog",
   };
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+  await api
+    .post("/api/blogs")
+    .set("Authorization", `bearer ${token}`)
+    .send(newBlog)
+    .expect(400);
 
   const blogsAtEnd = await helper.blogsInDb();
 
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  expect(blogsAtEnd).toEqual(blogsAtStart);
 });
 
 test("a blog can be deleted", async () => {
   const blogsAtStart = await helper.blogsInDb();
   const blogToDelete = blogsAtStart[0];
 
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .set("Authorization", `bearer ${token}`)
+    .expect(204);
 
   const blogsAtEnd = await helper.blogsInDb();
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
