b2fb0c9a13d0404c81963403085feab6223acecb
RotcivL <victor.lee.98.vic@gmail.com>
2023-01-24 20:44:45 +1100

    Ex.5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,28 +2,43 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+
+const userObject = {
+  id: 123123123123,
+  username: 'username',
+  name: 'name'
+}
+const blog = {
+  title: 'title',
+  author: 'author',
+  url: 'url',
+  likes: 2,
+  user: userObject
+}
 
 test('renders title and author but not url or likes by default', () => {
-  const user = {
-    id: 123123123123,
-    username: 'username',
-    name: 'name'
-  }
-  const blog = {
-    title: 'title rendered',
-    author: 'author rendered',
-    url: 'url not rendered',
-    likes: 2,
-    user: user
-  }
-
-  const { container } = render(<Blog blog={blog} user={user} />)
+  const { container } = render(<Blog blog={blog} user={userObject} />)
 
   const titleAuthor = container.querySelector('.blogTitleAuthor')
   expect(titleAuthor).toBeDefined()
-  expect(titleAuthor).toHaveTextContent('title rendered author rendered')
+  expect(titleAuthor).toHaveTextContent(`${blog.title} ${blog.author}`)
   expect(titleAuthor).not.toHaveStyle('display: none')
 
   const other = container.querySelector('.blogOther')
   expect(other).toHaveStyle('display: none')
+})
+
+test('renders url and likes when button is clicked', async () => {
+  const user = userEvent.setup()
+
+  const { container } = render(<Blog blog={blog} user={userObject}  />)
+
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const other = container.querySelector('.blogOther')
+  expect(other).not.toHaveStyle('display: none')
+  expect(other).toHaveTextContent(`${blog.url}`)
+  expect(other).toHaveTextContent(`likes ${blog.likes}`)
 })
