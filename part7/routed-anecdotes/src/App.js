f0ef51cb011ad94aea37bfd05d127d75a5513354
RotcivL <victor.lee.98.vic@gmail.com>
2023-01-31 17:30:12 +1100

    Ex.7.5

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -98,6 +98,12 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const reset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -114,8 +120,10 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='button' onClick={reset}>reset</button>
       </form>
+
     </div>
   )
 
